a5a1d04858cfc4814c2b322fb5349752f555683f
TrGiang-1210 <kobiet632@gmail.com>
2025-06-12 19:16:39 +0700

    Giang
    Đã xong giao diện trang sp và chi tiết sp

---
+++ frontend/src/data/products.ts
@@ -0,0 +1,103 @@
+export interface Product {
+  id: string;
+  name: string;
+  category: string;
+  price: number;
+  discountPrice?: number;
+  rating: number;
+  imageUrl: string;
+//   shortDescription: string;
+  isNew: boolean;
+}
+
+export const staticProducts: Product[] = [
+    {
+      id: '1',
+      name: 'CPU Intel Core i5 10400F',
+      category: 'intel',
+      price: 1980000,
+      discountPrice: 1650000,
+      rating: 4.5,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/i3_10400f_tray_820613579242482085e9f5330f541458_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.4GHz)',
+      isNew: false,
+    },
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+    
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+    {
+      id: '2',
+      name: 'CPU Intel Core i3 12100F',
+      category: 'intel',
+      price: 1490000,
+      discountPrice: 1290000,
+      rating: 4.2,
+      imageUrl: 'https://product.hstatic.net/1000288298/product/cpu_intel_core_i3_12100f_-_tray_new_pcm_cf0d559dbd3a4e17aad6f413e025b676_large.jpg',
+    //   shortDescription: 'TRAY NEW (3.3GHz)',
+      isNew: true,
+    },
+  ];
--- frontend/src/layouts/auth.layout.tsx
+++ frontend/src/layouts/auth.layout.tsx
@@ -1,16 +1,18 @@
-// import Footer from "@/components/footer/footer";
-// import Header from "@/components/header/header";
-// import React from "react";
-// import { Outlet } from "react-router-dom";
+// import Footer from "@/components/user/Footer";
+// import Header from "@/components/user/Header";
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-// const AuthLayout: React.FC = () => {
-//   return (
-//     <div>
-//       <Header />
-//       <Outlet /> 
-//       <Footer />
-//     </div>
-//   );
-// };
+const AuthLayout: React.FC = () => {
+  return (
+    <div className="user-layout">
+        <main className="user-content">
+        {/* <Header /> */}
+            <Outlet /> 
+        {/* <Footer /> */}
+        </main>
+    </div>
+  );
+};
 
-// export default AuthLayout;
+export default AuthLayout;
