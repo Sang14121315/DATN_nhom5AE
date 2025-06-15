0e01089176c6aeacadf19aae2696405827dbea5d
vanthien05 <vanthiendt05@gmail.com>
2025-06-15 19:31:53 +0700

    Thien
    update categoris model

--- backend/models/Category.js
+++ backend/models/Category.js
@@ -3,9 +3,10 @@ const mongoose = require('mongoose');
 const categorySchema = new mongoose.Schema({
   slug: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  description: String,
+  img_url: String,
+  parent: { type: mongoose.Schema.Types.ObjectId, ref: 'Category' }, // Thêm trường parent
   created_at: { type: Date, default: Date.now },
-  updated_at: { type: Date, default: Date.now }
+  updated_at: { type: Date, default: Date.now },
 });
 
 module.exports = mongoose.model('Category', categorySchema);
