0c9d3c8b547b496103a6fed8a7483848e1bb4384
vanthien05 <vanthiendt05@gmail.com>
2025-06-15 12:24:31 +0700

    Thien
    Update frontend/router

--- frontend/src/router/index.router.tsx
+++ frontend/src/router/index.router.tsx
@@ -1,32 +1,25 @@
 import { Routes, Route } from "react-router-dom";
 import AdminLayout from "@/layouts/admin.layout";
 import DashboardPage from "@/pages/admin/Dashboard";
-// import AuthLayout from "@/layouts/auth.layout";
-
-// import user 
 import AuthLayout from "@/layouts/auth.layout";
 import ProductlistPage from "@/pages/user/productList";
 import ProductdetailPage from "@/pages/user/productDetail";
 import HomePage from "@/pages/user/home";
 
-// Các page giả định
 const MainRouter = () => {
   return (
     <Routes>
-     
       {/* Admin layout */}
-      <Route path="/admin" element={< AdminLayout />}>
-      <Route path="dashboard" element={<DashboardPage />} />
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route path="dashboard" element={<DashboardPage />} />
       </Route>
 
-      {/* user layout */}
-      <Route path="/" element={< AuthLayout />}>
-      <Route path="home" element={ <HomePage />}/>
-      <Route path="productlist" element={<ProductlistPage />} />
-      <Route path="productdetail" element={<ProductdetailPage />} />
-
+      {/* User layout */}
+      <Route element={<AuthLayout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="productlist" element={<ProductlistPage />} />
+        <Route path="productdetail" element={<ProductdetailPage />} />
       </Route>
-      
     </Routes>
   );
 };
