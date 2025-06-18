bd97a402d5e59ad3c518e92f768682a43cba652c
TrGiang-1210 <kobiet632@gmail.com>
2025-06-18 18:17:06 +0700

    Giang
    trang ctsp: đã fix lỗi hiển thị phần sp liên quan, có thêm phần sp đã xem.
    trang sp: đã thêm phân trang, sửa 1 chút phần hiển thị giá.

--- frontend/src/router/index.router.tsx
+++ frontend/src/router/index.router.tsx
@@ -26,7 +26,7 @@ const MainRouter = () => {
         <Route index element={<HomePage />} />
         <Route path="home" element={<HomePage />} />
         <Route path="productlist" element={<ProductlistPage />} />
-        <Route path="productdetail" element={<ProductdetailPage />} />
+        <Route path="product/:id" element={<ProductdetailPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="register" element={<RegisterPage />} />
         <Route path="forgot-password" element={<ForgotPasswordPage />} />
