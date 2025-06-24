45d99ff216ccfe8fd23c149bc3288d2edd31a5f8
ngoctien261 <ngtien.2610@gmail.com>
2025-06-24 19:08:20 +0700

    router

--- frontend/src/router/index.router.tsx
+++ frontend/src/router/index.router.tsx
@@ -19,6 +19,9 @@
 
   import OrderTrackingPage from "@/pages/user/OrderTrackingPage";
 
+  import ContactPage
+   from "@/pages/user/ContactPage";
+
 
   const MainRouter = () => {
     return (
@@ -42,7 +45,7 @@
           <Route path="cart" element={<CartPage />} />
           <Route path="checkout" element={<CheckoutPage />} />
           <Route path="orders" element={<OrderTrackingPage />} />
-          
+          <Route path="contact" element={<ContactPage />} />
         </Route>
       </Routes>
     );
